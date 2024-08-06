c50a4e085c5e05c590fbd4c4df98dc24a0af1809
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-08-06 15:34:08 -0700

    done?

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,26 @@
 // Import the Spinner component into this file and test
+import React from 'react'
+import { render, waitFor, screen } from '@testing-library/react'
+//import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Spinner from './Spinner'
+
+
+
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+
+describe('Spinner Component', () => {
+
+// let user 
+//   beforeEach(() => {
+//     render(<Spinner />)
+//     user = userEvent.setup()
+//   })
+
+  test('Spinner renders without error', () => {
+    render (<Spinner on = {true}/>) 
+  })
+
 })
+
+
